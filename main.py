81f0f6eb3cfb8076d140e855976d26c2bf185769
akaNiknok <austingimperial@gmail.com>
2015-08-20 18:10:50 +0800

    Get salary from user

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 """Income Tax Calculator: A simple program that calculates users income tax"""
+
+# Get salary from user
+while True:
+    try:
+        salary = int(raw_input("Please enter your salary: "))
+        break
+    except ValueError:
+        print "Invalid input, please try again..."
