d081b14a8f3f6a34b5f776476b93c49a1989811a
akaNiknok <austingimperial@gmail.com>
2015-08-20 19:49:25 +0800

    Limit all lines to a maximum of 79 characters

--- main.py
+++ main.py
@@ -8,17 +8,26 @@ while True:
     except ValueError:
         print "Invalid input, please try again..."
 
+#TODO: Deduct SSS and Philhealth Premiums
+
+
 if salary <= 10000:
     print "Your salary is P" + format(salary * .95, ".2f")
 elif salary <= 30000:
-    print "Your salary is P" + format(salary - (((salary - 10000) * .10) + 500), ".2f")
+    print "Your salary is P" + \
+          format(salary - (((salary - 10000) * .10) + 500), ".2f")
 elif salary <= 70000:
-    print "Your salary is P" + format(salary - (((salary - 30000) * .15) + 2500), ".2f")
+    print "Your salary is P" + \
+          format(salary - (((salary - 30000) * .15) + 2500), ".2f")
 elif salary <= 140000:
-    print "Your salary is P" + format(salary - (((salary - 70000) * .20) + 8500), ".2f")
+    print "Your salary is P" + \
+          format(salary - (((salary - 70000) * .20) + 8500), ".2f")
 elif salary <= 250000:
-    print "Your salary is P" + format(salary - (((salary - 140000) * .25) + 22500), ".2f")
+    print "Your salary is P" + \
+          format(salary - (((salary - 140000) * .25) + 22500), ".2f")
 elif salary <= 500000:
-    print "Your salary is P" + format(salary - (((salary - 250000) * .30) + 50000), ".2f")
+    print "Your salary is P" + \
+          format(salary - (((salary - 250000) * .30) + 50000), ".2f")
 else:
-    print "Your salary is P" + format(salary - (((salary - 500000) * .32) + 125000), ".2f")
+    print "Your salary is P" + \
+          format(salary - (((salary - 500000) * .32) + 125000), ".2f")
